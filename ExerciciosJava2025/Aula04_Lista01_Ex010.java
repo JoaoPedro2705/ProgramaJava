066b11dd76b8c777b2b2e7a5509e0b4cc7b17fc8
João Pedro Antunes De Souza <teddbrgamer77@gmail.com>
2025-09-04 21:22:33 -0300

    Update Aula04_Lista01_Ex010.java

--- ExerciciosJava2025/Aula04_Lista01_Ex010.java
+++ ExerciciosJava2025/Aula04_Lista01_Ex010.java
@@ -9,30 +9,35 @@ public class Aula04_Lista01_Ex010 {
 
         Random ran = new Random();
         Scanner tec = new Scanner(System.in);
-
+        int alea = ran.nextInt(101); 
+        int tnt = 0;
+        int palp = 0;
+        
         System.out.println("=================================================");
         System.out.println("");
         System.out.println("              Adivinhe o Número                 ");
-        System.out.println("");
+        System.out.println("                Entre 1 e 100                   ");        
         System.out.println("=================================================");
         System.out.println("");
-        for (int i = 1; i <= 7; i++) {
+        for (tnt = 1; tnt <= 7; tnt++) {
             System.out.println("");
-            System.out.println("Informe o " + i + "° Número : ");
+            System.out.println("Tentativa " + tnt + " : ")
             int num = tec.nextInt();
             System.out.println("");
 
-            int sorteado = ran.nextInt(100) + 1; 
-            if (num == sorteado) {
-                System.out.println("Parabéns! Você acertou!");
-            } else if (num < sorteado) {
-                System.out.println("O número sorteado é maior!");
-            } else {
-                System.out.println("O número sorteado é menor!");
-            }
-        
             
+            if (num == alea) {
+                System.out.println("Parabéns! Você acertou em " + tnt + " tentativas !");
+                break;
+            } else if (num < alea) {
+                System.out.println("O número sorteado é MAIOR!");
+            } else {
+                System.out.println("O número sorteado é MENOR!");
+            }      
     }
+        if(num != alea){
+             System.out.println("Você não conseguiu adivinhar o número \nO número era " + alea);
+             System.out.println("O número sorteado era " + alea");
         
         }
             
